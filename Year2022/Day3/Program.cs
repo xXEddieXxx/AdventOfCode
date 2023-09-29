2e4906f53cff48cc455dacc1024ec75dc3cf58b5
Eddie <85015209+xXEddieXxx@users.noreply.github.com>
2023-09-29 10:22:30 +0200

    feat: add Day3 of 2022

--- Year2022/Day3/Program.cs
+++ Year2022/Day3/Program.cs
@@ -1,3 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿var body = File.ReadAllLines("Input.txt");
 
-Console.WriteLine("Hello, World!");
+//part1
+
+var alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+var compartment1 = "";
+var compartment2 = "";
+var points = 0;
+
+foreach (var line in body)
+{
+    compartment1 = line.Substring(0, line.Length/2);
+    compartment2 = line.Substring(line.Length/2,  line.Length/2);
+
+    foreach (var cha in compartment1)
+    {
+        if (compartment2.Contains(cha))
+        {
+            points += alphabet.IndexOf(cha) + 1;
+            break;
+        }
+    }
+}
+
+Console.WriteLine(points);
+
+//part 2 
+
+points = 0;
+var group = new List<string>();
+var counter = 0;
+foreach (var line in body)
+{
+    counter++;
+    group.Add(line);
+
+    if (counter == 3)
+    {
+        counter = 0;
+        foreach (var cha in group[0])
+        {
+            if (group[1].Contains(cha) && group[2].Contains(cha))
+            {
+                points += alphabet.IndexOf(cha) + 1;
+                break;
+            }
+        }
+        group.Clear();
+    }
+}
+
+Console.WriteLine(points);
